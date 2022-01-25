d7815562b0b67eb5323c02c3b606c0e44631d4e5
umburek <robert.kubica1982@gmail.com>
2022-01-25 13:58:23 +0100

    Added new columns to User table

---
+++ db/migrate/20220125124055_add_new_columns_to_users_table.rb
@@ -0,0 +1,7 @@
+class AddNewColumnsToUsersTable < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :role, :integer
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_25_103104) do
+ActiveRecord::Schema.define(version: 2022_01_25_124055) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2022_01_25_103104) do
     t.datetime "remember_created_at", precision: 6
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "role"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
