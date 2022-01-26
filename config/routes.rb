c969d32d99a53425fb2e4296a42d0c9e312a82eb
umburek <robert.kubica1982@gmail.com>
2022-01-26 11:43:00 +0100

    Added Admin create in Seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
-  devise_for :views
   root to: "home#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+admin = User.create(
+  email: 'admin@admin.com',
+  password: 'secret',
+  password_confirmation: 'secret',
+  role: :admin
+)
