e18ed6c1580284eef07f3b59f68dd1bcdc18d838
umburek <robert.kubica1982@gmail.com>
2022-02-21 11:30:43 +0100

    Added export CSV and XLS file for Admin

--- app/controllers/admin/purchases_controller.rb
+++ app/controllers/admin/purchases_controller.rb
@@ -4,6 +4,13 @@ module Admin
     def index
       @q = Purchase.all.where(status: :open).order(created_at: :desc).ransack(params[:q])
       @purchases_searched = @q.result(distinct: true).paginate(page: params[:page], per_page: 5)
+      @purchases = @q.result(distinct: true)
+
+      respond_to do |format|
+        format.html
+        format.csv { send_data @purchases.to_csv, filename: "purchases_#{Date.today}.csv" }
+        format.xls
+      end
     end
   end
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,15 @@
 class Purchase < ApplicationRecord
   has_many :items, class_name: 'PurchaseItem'
   belongs_to :user
+
+  def self.to_csv(options = {})
+    attributes = %w{ id sub_total created_at updated_at token status user_id }
+    CSV.generate(options.to_s, headers: true) do |csv|
+      csv << attributes
+
+      all.each do |purchase|
+        csv << purchase.attributes.values_at(*attributes)
+      end
+    end
+  end
 end
