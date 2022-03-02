8fcfab9e463410090828d1f939c7992c7ea6e698
umburek <robert.kubica1982@gmail.com>
2022-03-02 13:36:05 +0100

    Added Date Select to Statistics

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,3 +8,13 @@ import "controllers"
 //= require jquery.ui.datepicker-es
 //= require jquery.ui.datepicker-fr
 //= require bootstrap-datepicker
+
+$("input.datepicker").each(function(input) {
+    $(this).datepicker({
+        dateFormat: "yy-mm-dd",
+        altField: $(this).next()
+    })
+
+    // If you use i18n-js you can set the locale like that
+    $(this).datepicker("option", $.datepicker.regional[I18n.currentLocale()]);
+})
